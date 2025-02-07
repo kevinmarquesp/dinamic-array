19f3bb81ccbf42aaac37a69ccc450efde287c776
Kevin Marques <kevinmarqueskmp@gmail.com>
2025-02-07 12:51:20 -0300

    :bulb: docs-code: Documented the header file, now I started documenting the implementation code

--- include/darr.h
+++ include/darr.h
@@ -4,14 +4,56 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifndef DARR_DEFAULT_CAPACITY
+/*!
+ * Initial array capacity, that's just an arbitrary number and it can shrank as
+ * the items is removed. But this value can be changed to avoid resizing the
+ * array too soon.
+ */
 #define DARR_DEFAULT_CAPACITY 2
-#define DARR_DEFAULT_GROWING_FACTOR 1.25f
+#endif // !DARR_DEFAULT_CAPACITY
 
+#ifndef DARR_DEFAULT_GROWING_FACTOR
+/*!
+ * The growing factor will define how much the array should be resized when
+ * needed. Between 1 and 2 is a good choice. By the way, **this value should be
+ * a float**.
+ */
+#define DARR_DEFAULT_GROWING_FACTOR 1.25f
+#endif // !DARR_DEFAULT_GROWING_FACTOR
+
+/*!
+ * Type for the function that the `darr_find()` method will use to iterate over
+ * all items in the array to compare with the target item that needs to be
+ * found. **The user isn't meant to use this type**, it's useful to make the
+ * definition of the *find* paramter easier to write -- but it can also be used
+ * as reference to build the comparation function in the main code.
+ */
 typedef bool(compfun_t)(void const *curr, void const *value);
 
+/*!
+ * Struct that will **hold the actual dinamic array** and other informations to
+ * search, insert and remove elements from within. Since **C** doesn't support
+ * *generic types*, the array will be stored as an array that points to a
+ * sequence of pointers (I know complicated).
+ *
+ * Since all the items is a generic pointer (`void*`) to something, it's due to
+ * the developer to cast that item into a type that makes sence, for an example:
+ * `darr_at(mydarr, 1)` will be a `void*`, cast this `void` to a `int` with
+ * `(int *)darr_at(mydarr, 1)` to interprete this as a pointer to an integer,
+ * and now access the value that it's pointing to with an extra `*`, like `*(int
+ * *)darr_at(mydarr, 1)`.
+ *
+ * Check the detailed `darr_t` description for more information about it's
+ * attributes.
+ */
 typedef struct darr_t {
+  /*! Pointer to the array body, the elements should be casted when using. */
   void **_arr;
-  size_t _capacity, _size;
+  /*! Total ammount of spaces being used by the array, used in `for` loops. */
+  size_t _size;
+  /*! Max capacity, this should change when it needs to resize. */
+  size_t _capacity;
 } darr_t;
 
 darr_t *darr_new(void);
--- src/darr.c
+++ src/darr.c
@@ -5,6 +5,19 @@
 #include <string.h>
 #include <strings.h>
 
+bool _increase_darr(darr_t *da); //!< **private** method form *`darr_t`*
+bool _shrink_darr(darr_t *da);   //!< **private** method form *`darr_t`*
+
+/*!
+ * Creates a `darr_t` instance in the heap and allocates the initial size that
+ * the array will occupe based on the `DARR_DEFAULT_CAPACITY` value. The array
+ * will be a pointer [array] to other pointers with `void` (generic) type, this
+ * means that the items should be castet to some type when using, e.g.: `*(float
+ * *)da->_arr[1]` or, even better, `*(float *)darr_at(da, 1)`.
+ *
+ * @return A pointer to the created `darr_t` instance, it should be used in
+ * the other `darr_*()` methods to interact with the array itself.
+ */
 darr_t *darr_new(void) {
   darr_t *da = malloc(sizeof(darr_t));
 
@@ -15,36 +28,27 @@ darr_t *darr_new(void) {
   return da;
 }
 
+/*!
+ * Simple getter, intended to be used in `for`/`while` loops to interact with
+ * the array itself.
+ *
+ * @return The `darr_t._size` attribute.
+ * */
 size_t darr_get_size(darr_t *da) { return da->_size; }
 
+/*!
+ * Simple getter, this one was intended to be used for debugging propurses, but
+ * it can be in handy when needing to have fine control over the memory usage.
+ *
+ * @return The `darr_t._capacity` attribute.
+ */
 size_t darr_get_capacity(darr_t *da) { return da->_capacity; }
 
+/*!
+ *
+ */
 void *darr_at(darr_t *da, size_t index) {
   return index >= da->_size ? NULL : da->_arr[index];
-}
-
-bool _increase_darr(darr_t *da) {
-  if (da->_size + 1 <= da->_capacity)
-    return true;
-
-  __auto_type new_capacity =
-      (size_t)((float)da->_capacity * DARR_DEFAULT_GROWING_FACTOR);
-
-  new_capacity =
-      new_capacity == da->_capacity ? new_capacity + 1 : new_capacity;
-
-  void *temp = realloc(da->_arr, sizeof(void *) * new_capacity);
-
-  if (temp == NULL) {
-    fprintf(stderr, "_increase_darr(): realocation failed\n");
-
-    return false;
-  }
-
-  da->_arr = temp;
-  da->_capacity = new_capacity;
-
-  return true;
 }
 
 int darr_push(darr_t *da, void *value, size_t size) {
@@ -61,27 +65,6 @@ int darr_push(darr_t *da, void *value, size_t size) {
   ++da->_size;
 
   return index;
-}
-
-bool _shrink_darr(darr_t *da) {
-  if ((float)da->_size - 1 >=
-          (float)da->_capacity / DARR_DEFAULT_GROWING_FACTOR ||
-      da->_capacity <= DARR_DEFAULT_CAPACITY)
-    return true;
-
-  da->_capacity = da->_size;
-
-  void *temp = realloc(da->_arr, sizeof(void *) * da->_capacity + 1);
-
-  if (temp == NULL) {
-    fprintf(stderr, "_shrink_darr(): realocation failed\n");
-
-    return false;
-  }
-
-  da->_arr = temp;
-
-  return true;
 }
 
 int darr_pop(darr_t *da) {
@@ -157,4 +140,49 @@ void darr_destroy(darr_t *da) {
     free(da->_arr);
 
   free(da);
+}
+
+bool _increase_darr(darr_t *da) {
+  if (da->_size + 1 <= da->_capacity)
+    return true;
+
+  __auto_type new_capacity =
+      (size_t)((float)da->_capacity * DARR_DEFAULT_GROWING_FACTOR);
+
+  new_capacity =
+      new_capacity == da->_capacity ? new_capacity + 1 : new_capacity;
+
+  void *temp = realloc(da->_arr, sizeof(void *) * new_capacity);
+
+  if (temp == NULL) {
+    fprintf(stderr, "_increase_darr(): realocation failed\n");
+
+    return false;
+  }
+
+  da->_arr = temp;
+  da->_capacity = new_capacity;
+
+  return true;
+}
+
+bool _shrink_darr(darr_t *da) {
+  if ((float)da->_size - 1 >=
+          (float)da->_capacity / DARR_DEFAULT_GROWING_FACTOR ||
+      da->_capacity <= DARR_DEFAULT_CAPACITY)
+    return true;
+
+  da->_capacity = da->_size;
+
+  void *temp = realloc(da->_arr, sizeof(void *) * da->_capacity + 1);
+
+  if (temp == NULL) {
+    fprintf(stderr, "_shrink_darr(): realocation failed\n");
+
+    return false;
+  }
+
+  da->_arr = temp;
+
+  return true;
 }
